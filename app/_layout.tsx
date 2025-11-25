51126ad4786884ab343723e8b7c2943e3ce96bba
RomanCLV <roman.clavier.2001@gmail.com>
2025-11-25 16:07:55 +0100

    tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Stack - Page 1" }} />
-      <Stack.Screen name="page2" options={{ title: "Stack - Page 2" }} />
-      <Stack.Screen name="page3" options={{ title: "Stack - Page 3" }} />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: "Tabs - Page 1" }} />
+      <Tabs.Screen name="page2" options={{ title: "Tabs - Page 2" }} />
+      <Tabs.Screen name="page3" options={{ title: "Tabs - Page 3" }} />
+    </Tabs>
   );
 }
