be51d0c4ba86bb012c8bd8f621dd94a398d074b3
RomanCLV <roman.clavier.2001@gmail.com>
2025-11-26 14:22:22 +0100

    clear all

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,15 @@
 /*
 Expo Router :
 npx expo install expo-router react-native-safe-area-context react-native-screens expo-linking expo-constants expo-status-bar
+
+Vector Icons :
+npm install @expo/vector-icons --legacy-peer-deps
 */
 import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Stack - Page 1" }} />
-      <Stack.Screen name="page2" options={{ title: "Stack - Page 2" }} />
-      <Stack.Screen
-        name="page3"
-        options={{
-          title: "Stack - Page 3",
-          headerBackVisible: false, // enlève la flèche retour
-        }}
-      />
-      <Stack.Screen name="page4" options={{ title: "Stack - Page 4" }} />
-      <Stack.Screen
-        name="page5"
-        options={{
-          headerShown: false, // aucun header
-        }}
-      />
     </Stack>
   );
 }
