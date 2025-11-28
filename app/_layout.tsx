c1c21e1451dfe492044682326acc8105db27039e
RomanCLV <roman.clavier.2001@gmail.com>
2025-11-28 12:50:20 +0100

    clear package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ Expo Router :
 npx expo install expo-router react-native-safe-area-context react-native-screens expo-linking expo-constants expo-status-bar
 
 Vector Icons :
-npm install @expo/vector-icons --legacy-peer-deps
+npm install @expo/vector-icons
 */
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
